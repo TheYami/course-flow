e44bb2fc34cf014e4077cae19113a4d3bdeea643
Oat Chana <chanathip0944729842@gmail.com>
2024-12-11 16:57:03 +0700

    feat:render fetch courses data at my course page

---
+++ pages/mycourse/index.jsx
@@ -0,0 +1,11 @@
+import MyCourse from "@/components/mycourse/mycourse"
+import Navbar from "@/components/navbar"
+
+export default function MyCoursePage() {
+    return (
+        <div className="my-course-page">
+            <Navbar/>
+            <MyCourse/>
+        </div>
+    )
+}
