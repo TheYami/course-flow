50c662a7ab44090a1a3a34edb7834c0e7e00fb3c
podusit <d.rimdusit@gmail.com>
2024-12-16 11:19:07 +0700

    fix:merge conflict

---
+++ pages/course/[slug]/progress.jsx
@@ -0,0 +1,19 @@
+import CourseProgress from "@/components/course-progress";
+import Navbar from "@/components/navbar";
+import CollapsiblePanel from "@/components/collapsible-panel";
+import Footer from "@/components/footer";
+const CourseProgressPage = () => {
+  return (
+    <>
+    <Navbar />
+    <CourseProgress />
+    <CollapsiblePanel />
+    <Footer />
+    </>
+    
+  )
+}
+    
+
+export default CourseProgressPage;
+
