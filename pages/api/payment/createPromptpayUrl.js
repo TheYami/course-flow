2857113e2d1af5520b3d0abc521806822a54fee4
Oat Chana <chanathip0944729842@gmail.com>
2024-12-17 15:14:06 +0700

    feat:insert order data to supabase

--- pages/api/payment/createPromptpayUrl.js
+++ pages/api/payment/createPromptpayUrl.js
@@ -1,17 +1,21 @@
 import Stripe from "stripe";
+import { v4 as uuidv4 } from "uuid";
+import connectionPool from "@/utils/db";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const { courseId, amount, userId, refNumber, currency } = req.body;
+      const { courseId, amount, userId } = req.body;
+      const referenceNumber = uuidv4();
+
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["promptpay"],
         line_items: [
           {
             price_data: {
-              currency: currency,
+              currency: "thb",
               product_data: {
                 name: `Course ${courseId}`,
               },
@@ -24,18 +28,38 @@ export default async function handler(req, res) {
         success_url: `http://localhost:3000/payment/success-payment`,
         cancel_url: `http://localhost:3000/payment/failed-payment`,
         metadata: {
-          reference_number: refNumber,
+          reference_number: referenceNumber,
           user_id: userId,
           course_id: courseId,
           course_name: `Course ${courseId}`,
           amount,
-          currency,
         },
       });
-      res.status(200).json({ url: session.url });
+
+      const query = `
+      INSERT INTO orders (user_id, course_id, session_id, status, reference_number, created_at)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING reference_number;
+    `;
+
+      const values = [
+        userId,
+        courseId,
+        session.id,
+        session.status,
+        referenceNumber,
+        new Date(),
+      ];
+
+      const result = await connectionPool.query(query, values);
+      const insertedOrderId = result.rows[0].reference_number;
+
+      res.status(200).json({
+        url: session.url,
+        reference_number: insertedOrderId,
+      });
     } catch (error) {
-      console.error("Error creating Stripe session:", error);
-      res.status(500).json({ error: "Internal server error" });
+      res.status(500).json({ error: "Internal server error", error });
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
