17eff216e9ed3bc5e0c9d44c7f8f9569f99021b0
podusit <d.rimdusit@gmail.com>
2024-12-18 14:16:05 +0700

    fix:redirect to course  progress page

---
+++ components/mycourse/[slug]/progress.jsx
@@ -0,0 +1,17 @@
+import CourseProgress from "@/components/course-progress";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+const CourseProgressPage = () => {
+  return (
+    <>
+    <Navbar />
+    <CourseProgress />
+    <Footer />
+    </>
+    
+  )
+}
+    
+
+export default CourseProgressPage;
+
