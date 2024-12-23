5a1bf154dd715b937fbc3f5c7b4c9f4d3a393710
podusit <d.rimdusit@gmail.com>
2024-12-23 18:14:38 +0700

    fix:conflict

--- components/course-progress.jsx
+++ components/course-progress.jsx
@@ -286,7 +286,7 @@ export default function CourseProgress({slug}) {
           {/* Course Sections */}
           <div className="flex flex-col items-center">
             {/* Course Sections */}
-            {sections.map((section) => (
+            {/* {subcribeCoursesData.map((section) => (
               <div key={section.id} className="mb-4 w-full">
                 <CollapsiblePanel
                   title={
@@ -326,7 +326,7 @@ export default function CourseProgress({slug}) {
                   )}
                 </CollapsiblePanel>
               </div>
-            ))}
+            ))} */}
           </div>
         </section>
         {/*Right section */}
@@ -362,12 +362,11 @@ export default function CourseProgress({slug}) {
           onClick={handleNextLesson}
           disabled={
             selectedLessonIndex ===
-            sections.flatMap((section) => section.lessons).length - 1
+            subcribeCoursesData.flatMap((section) => section.lessons).length - 1
           }
         >
           Next Lesson
         </button>
       </div>
-    </>
-  );
+  </>;
 }
