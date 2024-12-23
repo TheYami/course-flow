a1c2164ee8540b2167612cfe3b6d63b891b7f5d5
podusit <d.rimdusit@gmail.com>
2024-12-23 17:55:01 +0700

    feat:course-learning fetch api

--- pages/api/course-learning/subscribeCourses.js
+++ pages/api/course-learning/subscribeCourses.js
@@ -5,15 +5,15 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { user_id, course_id } = req.query;
+  const { user_id, slug } = req.query;
 
   // ตรวจสอบว่ามีการส่ง `user_id` และ `slug` มาหรือไม่
   if (!user_id) {
     return res.status(400).json({ error: "Missing user_id" });
   }
 
-  if (!course_id) {
-    return res.status(400).json({ error: "Missing course_id" });
+  if (!slug) {
+    return res.status(400).json({ error: "Missing slug" });
   }
 
   const SqlStatement = `
@@ -28,21 +28,17 @@ export default async function handler(req, res) {
       lessons.lesson_name,
       sub_lessons.sub_lesson_id,
       sub_lessons.sub_lesson_name,
-      sub_lessons.complete_status,
       sub_lessons.video
     FROM subscriptions
     JOIN courses ON subscriptions.course_id = courses.course_id
     JOIN lessons ON courses.course_id = lessons.course_id
     JOIN sub_lessons ON lessons.lesson_id = sub_lessons.lesson_id
-    WHERE subscriptions.user_id = $1 AND courses.course_id = $2
+    WHERE subscriptions.user_id = $1 AND courses.slug = $2
   `;
 
   try {
     // ดึงข้อมูลจากฐานข้อมูล
-    const { rows } = await connectionPool.query(SqlStatement, [
-      user_id,
-      course_id,
-    ]);
+    const { rows } = await connectionPool.query(SqlStatement, [user_id, slug]);
 
     // ตรวจสอบว่าพบข้อมูลหรือไม่
     if (rows.length === 0) {
@@ -95,7 +91,7 @@ export default async function handler(req, res) {
     }, []);
 
     // ส่งข้อมูลกลับไปยัง client
-    return res.status(200).json(formattedData);
+    return res.status(200).json(rows);
   } catch (error) {
     console.error("Error fetching subscriptions:", error);
     return res.status(500).json({ error: "Internal server error" });
