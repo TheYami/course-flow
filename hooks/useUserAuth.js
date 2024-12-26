a791b14286f53d0cd62a1cd611e8c44d5b10f756
Oat Chana <chanathip0944729842@gmail.com>
2024-12-26 09:21:09 +0700

    feat:redirect to login page if guest try to access user's page

--- hooks/useUserAuth.js
+++ hooks/useUserAuth.js
@@ -1,53 +1,57 @@
-import { useEffect, useState } from 'react';
-import supabase from '@/lib/supabase';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import supabase from "@/lib/supabase";
+import { useRouter } from "next/router";
 
 const useUserAuth = () => {
-    const [user, setUser] = useState(null);
-    const [userData, setUserData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const router = useRouter();
-  
-    useEffect(() => {
-      const checkSession = async () => {
-        const { data: { session } } = await supabase.auth.getSession();
-  
-        if (!session) {
-          setUser(null);
-          setLoading(false);
-          return;
-        }
-  
-        setUser(session.user);
-        setLoading(false);
-  
-        const { data, error } = await supabase
-          .from('users')
-          .select('*')
-          .eq('email', session.user.email)
-          .single();
-  
-        if (error) {
-          console.error('Error fetching user data:', error);
-        } else {
-          setUserData(data); 
-        }
-      };
-  
-      checkSession();
-    }, [router]);
-    
-    useEffect(() => {
-      if (userData) {
-        if (userData.role !== "user") {
-          router.push("/login");
-        } else {
-          setLoading(false); 
-        }
-      }
-    }, [userData, router]);
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
-    return { userData, loading };
-  };
-  
-  export default useUserAuth;
+  useEffect(() => {
+    const checkSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (!session) {
+        setUser(null);
+        setLoading(false);
+        return;
+      }
+
+      setUser(session.user);
+      setLoading(false);
+
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("email", session.user.email)
+        .single();
+
+      if (error) {
+        console.error("Error fetching user data:", error);
+      } else {
+        setUserData(data);
+      }
+    };
+
+    checkSession();
+  }, [router]);
+
+  useEffect(() => {
+    if (userData) {
+      if (userData.role !== "user") {
+        router.push("/login");
+      } else {
+        setLoading(false);
+      }
+    } else {
+      router.push("/login");
+    }
+  }, [userData, router]);
+
+  return { userData, loading };
+};
+
+export default useUserAuth;
