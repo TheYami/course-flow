cc757a2052eadc88d101d5b2d54ff55bbe5e5ba7
Oat Chana <chanathip0944729842@gmail.com>
2025-01-13 11:35:09 +0700

    fix:wrong word

--- pages/api/my-course/getAllCourses.js
+++ pages/api/my-course/getAllCourses.js
@@ -20,6 +20,6 @@ export default async function handler(req, res) {
     );
     res.status(200).json(result.rows);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetcch courses", err: error });
+    res.status(500).json({ error: "Failed to fetch courses", err: error });
   }
 }
